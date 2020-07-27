87dba7a041d496fceb4a0558056e6f53805d48e8
Andre <jcsteele26@gmail.com>
2020-07-27 10:00:35 -0400

    Start Spring Boot OAuth Demo App

---
+++ springbootoauth/src/test/java/com/steele/springbootoauth/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steele.springbootoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
