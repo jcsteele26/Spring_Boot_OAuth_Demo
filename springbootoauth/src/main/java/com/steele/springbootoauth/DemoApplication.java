7ee95077a688e5a5862bea126a6d88392bed3e9d
Andre <jcsteele26@gmail.com>
2020-07-27 11:37:38 -0400

    Fix logout error

--- springbootoauth/src/main/java/com/steele/springbootoauth/DemoApplication.java
+++ springbootoauth/src/main/java/com/steele/springbootoauth/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,10 +34,11 @@ public class DemoApplication extends WebSecurityConfigurerAdapter{
             .authorizeRequests(a -> a
                 .antMatchers("/", "/error", "/webjars/**").permitAll()
 				.anyRequest().authenticated()
+			)
 			.logout(l -> l
 				.logoutSuccessUrl("/").permitAll() ) 
 			.csrf(c -> c 
-				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 			
             )
             .exceptionHandling(e -> e
